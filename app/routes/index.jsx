70ddd7619ef9ed12ca7e4aab289f89ec4d1adfc9
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-12-03 02:24:20 +0530

    Error Handling via ErrorBoundary

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -4,6 +4,7 @@ import homeStyles from "~/styles/home.css";
 export default function Index() {
   return (
     <main id="content">
+      <h1>Hello</h1>
       <h1>A better way of keeping track of your notes</h1>
       <p>Try our early beta and never loose track of your notes again!</p>
       <p id="cta">
--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,5 +1,5 @@
 import { redirect } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import NewNote, { links as newNoteLinks } from "~/components/NewNote";
 import NoteList, { links as noteLinks } from "~/components/NoteList";
 import { getStoredNotes, storeNotes } from "~/data/notes";
@@ -40,4 +40,16 @@ export async function action({ request }) {
 
 export function links() {
   return [...newNoteLinks(), ...noteLinks()];
+}
+
+export function ErrorBoundary({ error }) {
+  return (
+    <main className="error">
+      <h1>An error occurred in Notes</h1>
+      <p>{error.message}</p>
+      <p>
+        Back to <Link to="/">safety</Link>
+      </p>
+    </main>
+  );
 }
