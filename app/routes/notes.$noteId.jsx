e3fb71682f53d9b751fe6cce0d67edc570dafa27
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-12-03 23:57:55 +0530

    Another CatchBoundary Example

--- app/routes/notes.$noteId.jsx
+++ app/routes/notes.$noteId.jsx
@@ -1,3 +1,4 @@
+import { json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
 import { getStoredNotes } from "~/data/notes";
 
@@ -22,6 +23,12 @@ export async function loader({ params }) {
   const notes = await getStoredNotes();
   const noteId = params.noteId;
   const selectedNote = notes.find((note) => note.id == noteId);
+  if (!selectedNote) {
+    throw json(
+      { message: `Could not find note for id ${noteId}` },
+      { status: 404 }
+    );
+  }
   return selectedNote;
 }
 
